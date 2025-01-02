e0ebcfb24052115468ffe59195233fd93d044858
Zainab Hossien <ugs.19687@ci.suez.edu.eg>
2025-01-02 20:57:41 +0200

    models added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Create your views here.
+User = get_user_model()
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'accounts/user_list.html', {'users': users})
+
+def user_detail(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    return render(request, 'accounts/user_detail.html', {'user': user})
+
+def user_create(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/user_form.html', {'form': form})
+
+def user_update(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = CustomUserChangeForm(instance=user)
+    return render(request, 'accounts/user_form.html', {'form': form})
+
+def user_delete(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        user.delete()
+        return redirect('user_list')
+    return render(request, 'accounts/user_confirm_delete.html', {'user': user})
